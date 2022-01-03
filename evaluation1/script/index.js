be14300aa45cbd6c9aca24fd7a9a1e0a4ad07b3e
savagewebdev-stack <vantureprakash1903@gmail.com>
2022-01-03 18:37:29 +0530

    deleted some bugs

--- evaluation1/script/index.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#menu").addEventListener("onclick",menu);
-
-async function menu(){
-    let url = "www.themealdb.com/api/json/v1/1/random.php";
-    let api = await fetch(url);
-    let res = await api.json();
-    console.log(res);
-}
